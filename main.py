ff49a39d9c273c6aaa78fa22786a5338e4d7dc2d
Felipe Alencar <felipealencardev@gmail.com>
2024-01-25 11:40:24 -0300

    add function to return the number of words of a text

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    file_path = "./books/frankenstein.txt"
+    text = get_text_from_file(file_path)
+    num_words = get_num_words(text)
+    print(num_words)
+
+def get_text_from_file(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def get_num_words(text):
+    return len(text.split())
 
 main()
