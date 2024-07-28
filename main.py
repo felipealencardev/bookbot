a959d65b74f173f10dfce2e71f2ebb71408f4705
Felipe Alencar <felipealencardev@gmail.com>
2024-07-28 10:15:01 -0300

    add count charactesr dictionary and code refactoring

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = get_words_count(file_contents)
-        print(words)
+    file_path = "books/frankenstein.txt"
+    text = get_text_from_file(file_path)
+    words = get_words_count(text)
+    print(words)
+    char_dict = get_chars_dict(text)
+    print(char_dict)
 
-def get_words_count(file_contents):
-    return len(file_contents.split())
+def get_text_from_file(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def get_words_count(text):
+    return len(text.split())
+
+def get_chars_dict(text):
+    chars = {}
+    for word in text:
+        chars[word.lower()] = 0
+    for word in text:
+        chars[word.lower()] += 1
 
 main()
