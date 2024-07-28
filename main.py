5a227919064eafed615a3fb910ad02780b0d0406
Felipe Alencar <felipealencardev@gmail.com>
2024-07-28 10:34:48 -0300

    adding print report

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ def main():
     file_path = "books/frankenstein.txt"
     text = get_text_from_file(file_path)
     words = get_words_count(text)
-    print(words)
     char_dict = get_chars_dict(text)
-    print(char_dict)
+    char_sorted_list = convert_dict_list(char_dict)
+    print_report(file_path, words, char_sorted_list)
 
 def get_text_from_file(file_path):
     with open(file_path) as f:
@@ -19,5 +19,29 @@ def get_chars_dict(text):
         chars[word.lower()] = 0
     for word in text:
         chars[word.lower()] += 1
+    return chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def convert_dict_list(dict):
+    chars = []
+    for char in dict:
+        chars.append({"name": char, "num": dict[char]})
+    chars.sort(reverse=True, key=sort_on)
+    return chars
+
+def print_report(file_path, num_words, chars_list):
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for c in chars_list:
+        char = c["name"]
+        char_count = c["num"]
+        if char.isalpha():
+            print(f"The '{char}' character was found {char_count} times")
+    
+    print("--- End report ---")
 
 main()
